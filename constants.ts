d701089bf203ab2eacee38ff15da0a93471e3b7f
spservicesgroupinc-blip <spservicesgroupinc@gmail.com>
2026-02-03 06:14:50 -0500

    chore: Update GOOGLE_SCRIPT_URL to the new deployment address.

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
 
 // Web App URL from Google Apps Script Deployment
-export const GOOGLE_SCRIPT_URL: string = https://script.google.com/macros/s/AKfycbyIhr6_A9sxgb_3MniWoMVMAinLO4GLGohmk5P7B1xwfLu0VrQg60lWf1q2_iLN7JV7Ow/execzy2BKSp48NJrVQrw/exec'
+export const GOOGLE_SCRIPT_URL: string = 'https://script.google.com/macros/s/AKfycbxXpcjGTVBeWsQuBJQzsVOZxkxLBFkhwBIEMagTS2SK_azTeA69zsDW5bC6W6D5LgZSDg/exec'
