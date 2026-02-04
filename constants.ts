8f03fd8a78a7e90fa171c4cde1e55837160f0005
spservicesgroupinc-blip <spservicesgroupinc@gmail.com>
2026-02-04 17:33:32 -0500

    chore: Update the Google Script URL constant.

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
 
 // Web App URL from Google Apps Script Deployment
-export const GOOGLE_SCRIPT_URL: string = 'https://script.google.com/macros/s/AKfycbzOCwsBT1EQscP0doHN2SA9-JyCtQESVrUZ-5OYDHAb_3d9BnwjsAcaR8WErf1UPf8ggg/exec'
+export const GOOGLE_SCRIPT_URL: string = 'https://script.google.com/macros/s/AKfycbx833dameBfkB5ytjIafRZlTu6II7Z_UZYux7WqEYTMTm4BLRF3mIHsQ1loXTH9d9KELA/exec'
